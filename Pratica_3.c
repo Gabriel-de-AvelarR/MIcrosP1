a006819eb7ef2afcfd8f4af8c3f733db185df626
Gabriel-de-AvelarR <76533839+Gabriel-de-AvelarR@users.noreply.github.com>
2023-06-07 12:07:46 -0300

    Update Pratica_3.c
    Ta zuado

--- Pratica_3.c
+++ Pratica_3.c
@@ -1,4 +1,8 @@
-unsigned int temp_res;
+#define TAM 8
+float temp_res;
+float conversao;
+char stringConvert [TAM];
+unsigned int Vref =  5;
 //PINOS A SEREM SETADOS
 // Lcd pinout settings
 sbit LCD_RS at RB4_bit;
@@ -16,24 +20,38 @@ sbit LCD_D6_Direction at TRISB2_bit;
 sbit LCD_D5_Direction at TRISB1_bit;
 sbit LCD_D4_Direction at TRISB0_bit;
 
-
 void main() {
 
   ANSELA  = 0xFF;
   TRISA = 0XFF;
   
   ANSELB = 0;
-  TRISB = 0;
 
   ANSELC = 0;
   TRISC = 0;
   
+  ADC_Init();
   Lcd_Init();
-  Lcd_Out(1, 3, "Hello!");
+  
+  Lcd_Cmd(_LCD_CLEAR);               // Clear display
+  Lcd_Cmd(_LCD_CURSOR_OFF);          // Cursor off
+
 
   do {
-    temp_res = ADC_Read(1);   // Get 10-bit results of AD conversion
-    PORTB = temp_res;         // Send lower 8 bits to PORTB
-    PORTC = temp_res >> 8;    // Send 2 most significant bits to RC1, RC0
+    char str[TAM];
+    int conversaoPrim;
+    
+    temp_res = ADC_Read(0);   // Get 10-bit results of AD conversion
+    conversao = temp_res *  (5.0 / 1023.0) * 100.0;
+    intToStr(conversao, str);
+    str[0] = str[5];
+    str[3] = str[7];
+    str[2] = str[6];
+    str[1] = '.';
+    
+    Lcd_Out(1, 3, str);
   } while(1);
+  
+  
+  
 }
