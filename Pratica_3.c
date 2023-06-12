8aa18a4d29d5f4785059e3ee2bbaf7ef8cb83cf7
Gabriel-de-AvelarR <76533839+Gabriel-de-AvelarR@users.noreply.github.com>
2023-06-12 11:50:50 -0300

    Update Pratica_3.c
    Final

--- Pratica_3.c
+++ Pratica_3.c
@@ -1,8 +1,9 @@
 #define TAM 8
-float temp_res;
+unsigned int temp_res;
 float conversao;
 char stringConvert [TAM];
 unsigned int Vref =  5;
+
 //PINOS A SEREM SETADOS
 // Lcd pinout settings
 sbit LCD_RS at RB4_bit;
@@ -26,12 +27,12 @@ void main() {
   TRISA = 0XFF;
   
   ANSELB = 0;
+  TRISB = 0;
 
-  ANSELC = 0;
-  TRISC = 0;
-  
   ADC_Init();
   Lcd_Init();
+  
+ // Lcd_Out(1, 3, "Hello");
   
   Lcd_Cmd(_LCD_CLEAR);               // Clear display
   Lcd_Cmd(_LCD_CURSOR_OFF);          // Cursor off
@@ -41,15 +42,13 @@ void main() {
     char str[TAM];
     int conversaoPrim;
     
-    temp_res = ADC_Read(0);   // Get 10-bit results of AD conversion
-    conversao = temp_res *  (5.0 / 1023.0) * 100.0;
-    intToStr(conversao, str);
-    str[0] = str[5];
-    str[3] = str[7];
-    str[2] = str[6];
-    str[1] = '.';
+    temp_res = ADC_Get_Sample(0);   // Get 10-bit results of AD conversion
+    conversao = temp_res *  (5.0 / 1023.0);
+    sprintf(str, "%.2fV", conversao);
     
     Lcd_Out(1, 3, str);
+    
+    //delay_ms(1000);
   } while(1);
   
   
