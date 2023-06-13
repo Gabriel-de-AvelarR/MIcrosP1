2f054a8c52332cca9890188953fac772feef4c8b
David Dias <67483023+daviddias7@users.noreply.github.com>
2023-06-13 08:47:01 -0300

    Update Pratica_3.c
    Adicionados comentários, removido conversaoPrim, não usado

--- Pratica_3.c
+++ Pratica_3.c
@@ -1,6 +1,6 @@
 #define TAM 8
-unsigned int temp_res;
-float conversao;
+unsigned int temp_res; // Valor que será lido pelo conversor AD
+float conversao; // Valor float após a conversão do valor lido anteriormente
 
 //PINOS A SEREM SETADOS
 // Lcd pinout settings
@@ -21,32 +21,29 @@ sbit LCD_D4_Direction at TRISB0_bit;
 
 void main() {
 
-  ANSELA  = 0xFF;
-  TRISA = 0XFF;
+  ANSELA  = 0xFF; // Definindo porta A como analógica
+  TRISA = 0XFF; // Definindo Porta A como entrada
   
-  ANSELB = 0;
-  TRISB = 0;
+  ANSELB = 0; // Definindo porta B como digital
+  TRISB = 0; // Definindo porta B como saída
 
   ADC_Init();
   Lcd_Init();
-  
- // Lcd_Out(1, 3, "Hello");
   
   Lcd_Cmd(_LCD_CLEAR);               // Clear display
   Lcd_Cmd(_LCD_CURSOR_OFF);          // Cursor off
 
 
+   // Neste laço, o valor recebido pelo conversor AD é convertido para float e enviado ao display LCD
   do {
-    char str[TAM];
-    int conversaoPrim;
+    char str[TAM]; // string a ser enviada para o LCD
     
-    temp_res = ADC_Get_Sample(0);   // Get 10-bit results of AD conversion
-    conversao = temp_res *  (5.0 / 1023.0);
-    sprintf(str, "%.2fV", conversao);
+    temp_res = ADC_Get_Sample(0);   // Recebe o valor de 10 bits da conversão AD
+    conversao = temp_res *  (5.0 / 1023.0); // Conversão do valor inteiro para o valor float, segundo a resolução
+    sprintf(str, "%.2fV", conversao); // Transformar o valor float para string, com duas casas decimais
     
     Lcd_Out(1, 3, str);
     
-    //delay_ms(1000);
   } while(1);
   
   
